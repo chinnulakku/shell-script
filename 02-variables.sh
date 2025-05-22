79cfd68f3611bedfa6f960e6d8f85e166647b5c2
chinnulakku <chinnulakku4@gmail.com>
2025-05-22 15:42:47 +0530

    second program

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PERSON1=ram
-PERSONS2=RAJ
+PERSON2=RAJ
 echo "$PERSON1:Hello $PERSON2, Good Morning"
 echo "$PERSON2:Hii, $PERSON1, Very good Morning"
 echo "$PERSON1:How are You $PERSON2"
