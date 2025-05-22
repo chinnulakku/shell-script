aaf6add703132e1fb56b37fa0011c06e0f6c64ad
chinnulakku <chinnulakku4@gmail.com>
2025-05-22 17:21:37 +0530

    second program

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,8 +5,16 @@ ID=$(id -u)
 if [ %ID -ne 0 ]
 then
     echo"Error:: Please run this script with root access" 
+    exit 1 #you can give other than 0
 else
     echo "you are root user"
 fi
 
 yum install mysql -y
+
+if [$? -ne 0 ]
+then
+    echo "Installing MYSQL is fail"
+    exit 1
+else
+    echo "Installing MYSQL is success"
