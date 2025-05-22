54ae046df72f56e2a6671613706a3cecb8b844e7
chinnulakku <chinnulakku4@gmail.com>
2025-05-22 15:36:54 +0530

    second program

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "chinnu:Hello lakku, Good Morning"
-echo "lakku:Hii, Chinnu, Very good Morning"
-echo "chinnu:How are You lakku"
-echo "lakku:I am good chinnu, How are you"
+PERSON1=ram
+PERSONS=RAJ
+echo "$PERSON1:Hello $PERSON2, Good Morning"
+echo "$PERSON2:Hii, $PERSON1, Very good Morning"
+echo "$PERSON1:How are You $PERSON2"
+echo "$PERSON2:I am good $PERSON1, How are you"
