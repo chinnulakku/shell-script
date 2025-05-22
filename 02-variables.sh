6ad24f55762e02740b09d3c008270c6976c9710d
chinnulakku <chinnulakku4@gmail.com>
2025-05-22 15:32:00 +0530

    second program

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,4 +3,4 @@
 echo "chinnu:Hello lakku, Good Morning"
 echo "lakku:Hii, Chinnu, Very good Morning"
 echo "chinnu:How are You lakku"
-echo :"lakku:I am good chinnu, How are you"
+echo "lakku:I am good chinnu, How are you"
