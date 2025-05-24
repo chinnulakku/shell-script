1c7eef3f74804b310b584e436a08d3aa07d47cd4
chinnulakku <chinnulakku4@gmail.com>
2025-05-24 09:26:49 +0530

    packages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -12,7 +12,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    IF($1 -ne 0)
+    IF [ $1 -ne 0 ]
     then
         echo -e "$2 ...$R FAILED $N"
     else
