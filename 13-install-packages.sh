eb57a06af59935169977425af251292c047ace3c
chinnulakku <chinnulakku4@gmail.com>
2025-05-24 09:44:05 +0530

    packages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -12,12 +12,21 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    IF [ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
+    then
        echo -e "$2 ...$R FAILED $N"
     else
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end
 
 # echo "All arguments passed: $@"
 # git mysql postfix net-tools
